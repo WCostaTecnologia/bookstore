ec6f05b88c1cb9657c8bba8557e2efdbbb85d0ae
WCostaTecnologia <wesley1981@gmail.com>
2024-01-03 19:10:41 -0300

    finalizando as classes de dominio

--- src/main/java/com/wesley/bookstore/domain/Categoria.java
+++ src/main/java/com/wesley/bookstore/domain/Categoria.java
@@ -1,19 +1,26 @@
 package com.wesley.bookstore.domain;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class Categoria {
+public class Categoria  implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String descricao;
 
+    @OneToMany(mappedBy = "categoria")
     private List<Livro> livros = new ArrayList<>();
 }
--- src/main/java/com/wesley/bookstore/domain/Livro.java
+++ src/main/java/com/wesley/bookstore/domain/Livro.java
@@ -1,20 +1,29 @@
 package com.wesley.bookstore.domain;
 
+import
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class Livro {
+public class Livro implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String titulo;
     private String nome;
     private String text;
 
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
     private Categoria categoria;
 }
